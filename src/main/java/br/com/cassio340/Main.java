e18b54ab79aca298c81ac1460a6d1f8830602c72
cassio <cassiorodrigues340@gmail.com>
2026-05-14 12:30:56 -0300

    Adiciona classe InimigoFactory para geração de inimigos por nivel

--- src/main/java/br/com/cassio340/Main.java
+++ src/main/java/br/com/cassio340/Main.java
@@ -1,9 +1,12 @@
 package br.com.cassio340;
 
 
+import br.com.cassio340.entities.inimigos.NivelDificuldade;
+import br.com.cassio340.factory.InimigoFactory;
+
 public class Main {
     static void main() {
-
+        System.out.println(InimigoFactory.gerarInimigo(NivelDificuldade.FACIL));
     }
 }
 
---
+++ src/main/java/br/com/cassio340/factory/InimigoFactory.java
@@ -0,0 +1,36 @@
+package br.com.cassio340.factory;
+
+import br.com.cassio340.entities.inimigos.Inimigo;
+import br.com.cassio340.entities.inimigos.NivelDificuldade;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class InimigoFactory {
+    private static final Random rnd = new Random();
+    private static final List<Inimigo> inimigos = carregarInimigos();
+
+
+    private static List <Inimigo> carregarInimigos(){
+        String caminho = "src//main//resources//inimigos.json";
+        return LeitorJson.lerJson(caminho,Inimigo.class);
+    }
+
+
+    public static Inimigo gerarInimigo(NivelDificuldade nivel){
+        List <Inimigo> inimigosNivelados = new ArrayList<>();
+
+        for (Inimigo inimigo : inimigos){
+            if(inimigo.getNivel() == nivel){
+
+                inimigosNivelados.add(inimigo);
+            }
+        }
+        if (inimigosNivelados.isEmpty()){
+            throw new IllegalArgumentException("Nenhum inimigo encontrado para o nivel");
+        }
+        Inimigo inimigo = inimigosNivelados.get (rnd.nextInt(inimigosNivelados.size()));
+        return inimigo;
+
+    }
+}
